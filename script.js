1327c6ede2bfa8be7c6aa3ec62459212e076cb72
Doran-28 <116115557+Doran-28@users.noreply.github.com>
2024-07-11 11:18:25 -0400

    Implemented The game loop

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ let computerScore = 0;
 
 
 function getUserInput(){
-    let userInput = prompt("Rock (R), Paper (P), or Scissors (S): ");
-    if (userInput.toLowerCase() != "r" && userInput.toLowerCase() != "p" && userInput.toLowerCase() != "s" ){
+    let userInput = String(prompt("Rock (R), Paper (P), or Scissors (S): "));
+
+    while (userInput.toLowerCase() != "r" && userInput.toLowerCase() != "p" && userInput.toLowerCase() != "s" ){
         console.log("Incorrect value");
-        getUserInput();
+        userInput = String(prompt("Rock (R), Paper (P), or Scissors (S): "));
     }
     return userInput;
 }
@@ -18,5 +19,36 @@ function generateComputerInput(){
     return options[index];
 }
 
+function playRound(){
+    let userPlay = getUserInput();
+    let computerPlay = generateComputerInput();
+    let winner = ""
 
-console.log(generateComputerInput())
+    if (userPlay == computerPlay){
+        console.log("There has been a tie, no points gained")
+    }
+    else{
+        if (userPlay == 'r' && computerPlay == 's') {userScore++; winner = 'u';}
+        if (userPlay == 'r' && computerPlay == 'p') {computerScore++; winner = 'c';}
+        if (userPlay == 'p' && computerPlay == 'r') {userScore++; winner = 'u';}
+        if (userPlay == 'p' && computerPlay == 's') {computerScore++; winner = 'c';}
+        if (userPlay == 's' && computerPlay == 'p') {userScore++; winner = 'u';}
+        if (userPlay == 's' && computerPlay == 'r') {computerScore++; winner = 'c';}
+    }
+    console.log("User: " + userPlay + "  Computer: " + computerPlay)
+    winner == 'u' ? console.log("You win this round!") : console.log("You lose this round");
+}
+
+function game(){
+    while (userScore < 3 && computerScore < 3){
+        playRound();
+        console.log("Current score: User: " + userScore + " Computer: " + computerScore)
+    }
+    if (userScore > computerScore){
+        console.log("You win!");
+    }
+    else {
+        console.log("You lose");
+    }
+}
+game();
