d706b19d8bf2315e49b1802358908b39953bbffb
Doran-28 <116115557+Doran-28@users.noreply.github.com>
2024-07-11 10:18:22 -0400

    Implemented getUSerInput()

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 
-console.log("hello")
+let userScore = 0;
+let computerScore = 0;
+
+
+function getUserInput(){
+    let userInput = prompt("Rock (R), Paper (P), or Scissors (S): ");
+    if (userInput.toLowerCase() != "r" && userInput.toLowerCase() != "p" && userInput.toLowerCase() != "s" ){
+        console.log("Incorrect value");
+        getUserInput();
+    }
+    return userInput;
+}
+
+
+getUserInput();
